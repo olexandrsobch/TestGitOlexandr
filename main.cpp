aa9726fe54fbbc3f4028c230a396ef724a360f0f
Dima <livitorskipunovich@gmail.com>
2022-09-24 18:47:29 +0300

    done task 1

--- main.cpp
+++ main.cpp
@@ -3,5 +3,10 @@ using namespace std;
 
 int main()
 {
+	cout << "Name\t\t: \"The war and the peace\"" << endl;
+	cout << "Avtor\t\t: L.N. Tolstoj" << endl;
+	cout << "Izdatelstvo\t: Piter" << endl;
+	cout << "Pages\t\t: 500" << endl;
+
 	return 0;
 }
